48e4fb6f0bc4033a7bf4a7c0e13c247aa5cb9441
Raul <email@email.com>
2021-06-14 16:33:24 +0300

    new change

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-    cookieKeys:['dasfasfasf','asfasfasff'],
-    dbURL:'mongodb+srv://rauladmin:test123@firstcluster.v5ht7.mongodb.net/usertesting-dev?retryWrites=true&w=majority'
+if(process.env.NODE_ENV === 'production'){
+    module.exports = require('./prod');
+} else {
+    module.exports = require('./dev');
 }
---
+++ config/prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+    cookieKeys: process.env.COOKIE_KEYS,
+    dbURL: process.env.DB_URL
+}
